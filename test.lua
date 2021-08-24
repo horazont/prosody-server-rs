f76782f4f0af749313f2be9cbc93c8e58e95c36a
Jonas Schäfer <j.wielicki@sotecware.net>
2021-08-24 16:58:28 +0200

    Add a "post from within the loop" test in test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,12 @@
 local server = require "librserver".server;
+local ctr = 0;
 sender = server.test_mkecho({
 	onreceived = function(text)
 		print("got "..text);
+		ctr = ctr + 1;
+		if ctr < 2 then
+			sender:send("magic!");
+		end
 	end;
 });
 sender:send("test1");
